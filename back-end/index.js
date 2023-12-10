5632925bbf721c7545e3a19ad6b876f351c68dea
lehuyhaianh0808@gmail.com <lehuyhaianh0808@gmail.com>
2023-12-10 17:26:05 +0700

    vercel

--- back-end/index.js
+++ back-end/index.js
@@ -14,7 +14,7 @@ const { Server } = require("socket.io");
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://picture-selling-ychv.vercel.app/shopping"],
     methods: ["GET", "POST"],
     // allowedHeaders: ["my-custom-header"],
   },
@@ -24,6 +24,9 @@ var corsOptions = {
   // optionSuccessStatus: 200,
   // methods: ['AAA'],
   methods: "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+  origin: "https://picture-selling-ychv.vercel.app/shopping/",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 // app.use(function (req, res, next) {
